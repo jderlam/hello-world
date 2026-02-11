fe5ff8fe1b5b9826f57ef8a416b321d1911e6b63
jderlam <j.derlam@share.epsb.ca>
2026-02-11 10:53:38 -0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName(hungry);
+let weather = sunny
+
+if (weather == sunny) {
+    console.log("What a sunny day today")
+} else {
+    console.log("It's not a good time to go play outside")
+}
